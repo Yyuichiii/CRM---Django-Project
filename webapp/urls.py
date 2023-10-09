2155c9e1dda8c8355656408ceb05ee308df55c3b
Ritik <ritikmehradec@gmail.com>
2023-10-09 13:36:22 +0530

    Read and delete function is completed with messgage framework

--- webapp/urls.py
+++ webapp/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('logout/',views.user_logout, name='logout'),
     path('dashboard/',views.dashboard, name='dashboard'),
     path('create/',views.create_record, name='create'),
+    path('delete/<int:pk>',views.delete_record, name='delete'),
+    path('update/<int:pk>',views.update_record, name='update'),
+    path('view/<int:pk>',views.view_record, name='view'),
 ]
 
 
--- webapp/views.py
+++ webapp/views.py
@@ -4,6 +4,7 @@ from .forms import register_form,login_form,CreateRecordForm,UpdateRecordForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from .models import Record
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -15,6 +16,7 @@ def register(request):
         fm=register_form(request.POST)
         if fm.is_valid():
             fm.save()
+            messages.success(request, "Your Account has been successfully created")
             return redirect('login')
     
     
@@ -55,6 +57,7 @@ def login_(request):
 # User logout
 def user_logout(request):
     logout(request)
+    messages.success(request, "You are logout successfully")
     return redirect('login')
 
 
@@ -76,7 +79,37 @@ def create_record(request):
 
         if fm.is_valid():
             fm.save()
+            messages.success(request, "The Record has been created successfully")
 
             return redirect('dashboard')
         
     return render(request,'webapp/create-record.html',{'form':fm})
+
+
+
+@login_required(login_url='login')
+def update_record(request,pk):
+    record=Record.objects.get(id=pk)
+    fm=UpdateRecordForm(instance=record)
+    if request.method=='POST':
+        fm=UpdateRecordForm(request.POST,instance=record)
+
+        if fm.is_valid():
+            fm.save()
+            messages.success(request, "The Record has been updated successfully")
+
+            return redirect('dashboard')
+        
+    return render(request,'webapp/update-record.html',{'form':fm})
+
+@login_required(login_url='login')
+def view_record(request,pk):
+    record=Record.objects.get(id=pk)
+    return render(request,'webapp/view-record.html',{'record':record})
+
+@login_required(login_url='login')
+def delete_record(request,pk):
+    record=Record.objects.get(id=pk)
+    record.delete()
+    messages.success(request, "The Record has been deleted successfully")
+    return redirect('dashboard')
